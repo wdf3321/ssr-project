61c2646df2abc8eec5e2ad0b044c37a835e4babe
wdf <wdf332@gmail.com>
2023-03-19 14:16:27 +0800

    upload

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="bg-teal-6 text-white" reveal>
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-icon name="medical_services" size="xl" class=""></q-icon>
+          <q-btn
+            flat
+            size="lg"
+            to="#"
+            class="indexbtn"
+            style="padding-top: 10px"
+            >泰山耳鼻喉科
+          </q-btn>
+        </q-toolbar-title>
+
+        <q-drawer
+          class="lt-md text-center bg-teal"
+          v-model="rightDrawerOpen"
+          side="right"
+          bordered
+        >
+          <q-tabs class="lt-md text-white" vertical v-model="tab">
+            <q-tab name="home" label="HOME" href="#" />
+            <q-tab name="skill" label="SKILL" href="#skill" />
+            <q-tab name="about" label="ABOUT" href="#about" />
+            <q-tab name="portfolio" label="PORTFOLIO" href="#portfolio" />
+            <q-tab name="contact" label="CONTACT" href="#contact" />
+          </q-tabs>
+        </q-drawer>
+        <q-tabs class="gt-sm" v-model="tab">
+          <q-tab name="home" label="HOME" href="#" />
+          <q-tab name="skill" label="SKILL" href="#skill" />
+          <q-tab name="about" label="ABOUT" href="#about" />
+          <q-tab name="portfolio" label="PORTFOLIO" href="#portfolio" />
+          <q-tab name="contact" label="CONTACT" href="#contact" />
+        </q-tabs>
+
+        <q-btn
+          flat
+          icon="menu"
+          @click="toggleRightDrawer"
+          class="lt-md q-pt-xs"
+          size="md"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const tab = ref('top')
+const rightDrawerOpen = ref(false)
+
+function toggleRightDrawer () {
+  rightDrawerOpen.value = !rightDrawerOpen.value
+}
+
+</script>
---
+++ src/pages/FirstPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <section id="home">
+    <div class="row">
+
+    </div>
+  </section>
+<section id="skill">
+<div class="row">
+  <img src="https://picsum.photos/1920/1080/?random=10">
+</div>
+</section>
+<section id="about">
+<div class="row">
+  <img src="https://picsum.photos/1920/1080/?random=9">
+</div>
+</section>
+<section id="portfolio">
+<div class="row">
+  <img src="https://picsum.photos/1920/1080/?random=8">
+</div>
+</section>
+<section id="contact">
+<div class="row">
+  <img src="https://picsum.photos/1920/1080/?random=7">
+</div>
+</section>
+</template>
+
+<script lang="ts">
+
+</script>
